a36ad04493ce7906672b1e6bbd9aeeabaf434fb4
churchill <churrrach@gmail.com>
2023-06-20 18:47:13 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet_x10 - print characters a-z
+*/
+void print_alphabet_x10(void);
+{
+int i = 0;
+int j = 97;
+while (i < 10)
+{
+while (j < 123)
+{
+_putchar(i);
+j++;
+}
+i++;
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _islower - print characters a-z
+* Return - int
+* c - value to check ascii
+*/
+int _islower(int c)
+{
+if (islower(c))
+{
+return (1);
+}
+return (0);
+}
