f41be42e53375fc1816fb346fdff4b25816e0376
churchill <churrrach@gmail.com>
2023-06-21 20:54:13 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,13 @@ if (n <= 15 && n >= 0)
 {
 for (digit = 0; digit <= n; digit++)
 {
-_putchar('0');
 for (mult = 0; mult <= n; mult++)
 {
+if (mult == 0)
+{
+_putchar('0');
+continue;
+}
 _putchar(',');
 _putchar(' ');
 result = digit * mult;
