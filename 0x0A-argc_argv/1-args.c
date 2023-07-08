aa8048960958ab635a618f42070180f4c61c5bc7
churchill <churrrach@gmail.com>
2023-07-07 20:44:49 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - gets the lenght
+* @argv: arg vector
+* @argc: arg count
+* Return: int
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", (argc - 1));
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Arguments values and argument counts
