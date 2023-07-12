8260d61b46207c036cab0da1f31879c9b969e5a8
churchill <churrrach@gmail.com>
2023-07-11 20:28:48 -0700

    update

--- 0x0B-malloc_free/che.c
+++ 0x0B-malloc_free/che.c
@@ -71,3 +71,32 @@ k++;
 }
 return p;
 }
+
+void print_tab(char **tab)
+{
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
+}
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(void)
+{
+    char **tab;
+
+    tab = strtow(" !@#$^&*(())_Talk ><><.., ][{} 76478 is ~~ cheap :) . Show #$45&*(45 me 873589 the |[]{} -_=+ code.^& ");
+    if (tab == NULL)
+    {
+        printf("Failed\n");
+        return (1);
+    }
+    print_tab(tab);
+  
+}
