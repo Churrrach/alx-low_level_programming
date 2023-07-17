6b35d534d418d723138a1285ee22a15a21566c5a
churchill <churrrach@gmail.com>
2023-07-17 14:40:38 -0700

    update on preprocessor and macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - gets string length
+* Return: 0
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Basics on preprocessor and macros
